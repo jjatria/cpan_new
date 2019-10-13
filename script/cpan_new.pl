82018abb1847b564feddc03b66d4ecdabb9d9203
José Joaquín Atria <jjatria@gmail.com>
2019-10-13 15:52:50 +0100

    Simplify adding timers to the loop

--- script/cpan_new.pl
+++ script/cpan_new.pl
@@ -34,68 +34,69 @@ my $loop = IO::Async::Loop->new;
 my $ua = Net::Async::HTTP->new( fail_on_error => 1 );
 $loop->add($ua);
 
-my $timer = IO::Async::Timer::Periodic->new(
-    first_interval => 1,
-    interval       => 30,
-    on_tick        => sub {
-        $log->trace('Fetching feed');
-        $ua->do_request(
-            uri => 'https://metacpan.org/feed/recent',
-            on_response => sub {
-                $log->trace('Got feed');
-                my $res = shift;
 
-                my $dom;
-                try {
-                    my $body = $res->content;
-                    open my $fh, '<', \$body;
-                    $dom = XML::Tiny::DOM->new($fh);
-                }
-                catch {
-                    $log->errorf('Could not parse XML: %s', $_);
-                    $log->debug( $res->content );
-                    return;
-                }
+$loop->add(
+    IO::Async::Timer::Periodic->new(
+        first_interval => 1,
+        interval       => 30,
+        on_tick        => sub {
+            $log->trace('Fetching feed');
+            $ua->do_request(
+                uri => 'https://metacpan.org/feed/recent',
+                on_response => sub {
 
-                for my $item ( $dom->item('*') ) {
-                    my $item_timestamp = Time::Piece
-                        ->strptime( $item->${\'dc:date'}, '%Y-%m-%dT%H:%M:%SZ' )
-                        ->epoch;
+                    $log->trace('Got feed');
+                    my $res = shift;
 
-                    $log->tracef(
-                        'Found %s posted at %s',
-                        $item->title,
-                        $item->${\'dc:date'},
-                    );
-
-                    if ( latest_timestamp() >= $item_timestamp ) {
-                        $log->tracef('Post is too old');
-                        next;
+                    my $dom;
+                    try {
+                        my $body = $res->content;
+                        open my $fh, '<', \$body;
+                        $dom = XML::Tiny::DOM->new($fh);
+                    }
+                    catch {
+                        $log->errorf('Could not parse XML: %s', $_);
+                        $log->debug( $res->content );
+                        return;
                     }
 
-                    latest_timestamp($item_timestamp);
+                    for my $item ( $dom->item('*') ) {
+                        my $item_timestamp = Time::Piece
+                            ->strptime( $item->${\'dc:date'}, '%Y-%m-%dT%H:%M:%SZ' )
+                            ->epoch;
 
-                    my $title = sprintf '%-.80s', $item->title;
+                        $log->tracef(
+                            'Found %s posted at %s',
+                            $item->title,
+                            $item->${\'dc:date'},
+                        );
 
-                    toot(
-                        sprintf "%s by %s\n%s\n%s",
-                        $title,
-                        $item->${\'dc:creator'},
-                        $item->description // '',
-                        $item->link
-                    );
-                }
-            },
-            on_error => sub {
-                $log->trace('Error fetching feed');
-                $log->warn( Dumper shift );
-            },
-        );
-    },
+                        if ( latest_timestamp() >= $item_timestamp ) {
+                            $log->tracef('Post is too old');
+                            next;
+                        }
+
+                        latest_timestamp($item_timestamp);
+
+                        my $title = sprintf '%-.80s', $item->title;
+
+                        toot(
+                            sprintf "%s by %s\n%s\n%s",
+                            $title,
+                            $item->${\'dc:creator'},
+                            $item->description // '',
+                            $item->link
+                        );
+                    }
+                },
+                on_error => sub {
+                    $log->trace('Error fetching feed');
+                    $log->warn( Dumper shift );
+                },
+            );
+        },
+    )->start
 );
-
-$timer->start;
-$loop->add($timer);
 
 {
     my @QUEUE;
@@ -114,18 +115,16 @@ $loop->add($timer);
         }
     }
 
-    my $watcher = IO::Async::Timer::Periodic->new(
-        first_interval => 5,
-        interval       => 300,
-        on_tick        => sub {
-            my $string = shift @QUEUE;
-            toot($string) if $string;
-        },
+    $loop->add(
+        IO::Async::Timer::Periodic->new(
+            first_interval => 5,
+            interval       => 300,
+            on_tick        => sub {
+                my $string = shift @QUEUE;
+                toot($string) if $string;
+            },
+        )->start
     );
-
-    $watcher->start;
-
-    $loop->add($watcher);
 }
 
 $log->debug('Start crawling');
